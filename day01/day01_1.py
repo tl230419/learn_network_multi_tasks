dad0e90121ba22e64df5d8dc2ad3217b04b8e2ad
Allen <tl230419@163.com>
2020-04-20 00:40:55 +0800

    update day01 08 logic

--- day01/day01_1.py
+++ day01/day01_1.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 
     udp_socket.close()
 '''
-
+'''
 from socket import *
 
 udp_socket = socket(AF_INET, SOCK_DGRAM)
@@ -45,5 +45,17 @@ recv_data = udp_socket.recvfrom(1024)
 
 print(recv_data[0].decode('gbk'))
 print(recv_data[1])
+
+udp_socket.close()
+'''
+
+'''------------08-----------'''
+import socket
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+addr = ("192.168.1.104", 7890)
+udp_socket.bind(addr)
+
+udp_socket.sendto("hello".encode(), ("192.168.1.102", 8080))
 
 udp_socket.close()
