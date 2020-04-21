edd7b8f8b18fac968c260aec5d7ac8d2295a5ac1
Allen <tl230419@163.com>
2020-04-21 09:37:34 -0700

    fix decode chinese by os version

--- day02/day02-2.py
+++ day02/day02-2.py
@@ -6,13 +6,17 @@ Author: Allen
 '''
 
 import socket
+import platform
 
 tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_client_socket.connect(("192.168.1.102", 7878))
+tcp_client_socket.connect(("192.168.146.129", 7878))
 tcp_client_socket.send("哈哈哈，打不过我吧？！".encode("utf-8"))
 
 recv_data = tcp_client_socket.recv(1024)
-# Chinese can not use utf-8 format decode!!!
-print("接收到数据：", recv_data.decode("gbk"))
+sysstr = platform.system()
+if sysstr == "Windows":
+    print("接收到数据：", recv_data.decode("gbk"))
+elif sysstr == "Linux":
+    print("接收到数据：", recv_data.decode("utf-8"))
 
 tcp_client_socket.close()
